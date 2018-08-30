5b424c96b11abda9519511ac0c9a843089e07263
AdrianTodt <adriantodt@outlook.com>
2018-08-30 11:47:20 -0300

    updated deps

--- src/main/kotlin/pw/aru/exported/exported.kt
+++ src/main/kotlin/pw/aru/exported/exported.kt
@@ -10,9 +10,9 @@ package pw.aru.exported
 /**
  * Aru! Version
  */
-const val aru_version = "2.3.1"
+const val aru_version = "2.4"
 
 /**
  * User Agent
  */
-const val user_agent = "JDA/DiscordBot (Aru! 2.3.1)"
+const val user_agent = "JDA/DiscordBot (Aru! 2.4)"
