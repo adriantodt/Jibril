6b98e26139cd26aafaa1158639d1eb57540f6bb7
AdrianTodt <adriantodt@outlook.com>
2019-05-18 10:42:46 -0300

    Welcome to LTS. With actual Docker and Dicefix.

--- src/main/kotlin/pw/aru/commands/funny/dice/ShadowDice.kt
+++ src/main/kotlin/pw/aru/commands/funny/dice/ShadowDice.kt
@@ -2,14 +2,24 @@
 
 package pw.aru.commands.funny.dice
 
-import pw.aru.utils.extensions.threadLocalRandom
+import pw.aru.utils.TaskManager.schedule
+import java.lang.System.nanoTime
+import java.util.*
+import java.util.concurrent.TimeUnit.MILLISECONDS
 import kotlin.math.max
 import kotlin.math.min
 
 class ShadowDice {
+    val r = Random()
+
+    init {
+        schedule(65536, MILLISECONDS) { r.setSeed(3447679086515839964L xor nanoTime()) }
+    }
+
     private inline fun Int.clamp(min: Int, max: Int) = min(max(this, min), max)
 
-    fun roll(sides: Int) = threadLocalRandom().run {
-        ((nextDouble() * 0.9 + nextDouble() * nextDouble() * nextDouble() * 0.8) * sides).toInt().clamp(0, sides - 1) + 1
+    fun roll(sides: Int) = r.run {
+        ((nextDouble() * 0.9 + nextDouble() * nextDouble() * nextDouble() * 0.8) * sides).toInt()
+            .clamp(0, sides - 1).plus(1)
     }
 }
