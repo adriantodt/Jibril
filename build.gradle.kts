a5d271fc08311bf6eccef330b20d7bde6af339a8
adriantodt <adrian.todtsilva@gmail.com>
2019-07-30 12:01:54 -0300

    lavaplayer bump, let's go

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     // Main APIs
     compile("com.github.natanbc:weeb4j:3.5")
     compile("pw.aru.libs:andeclient:1.5.1")
-    compile("com.sedmelluq:lavaplayer:1.3.17") {
+    compile("com.sedmelluq:lavaplayer:1.3.19") {
         exclude(group = "com.sedmelluq", module = "lavaplayer-natives")
     }
 
